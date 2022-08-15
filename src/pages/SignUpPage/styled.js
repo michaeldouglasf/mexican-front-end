700c13b9d07b4c90a13ddef8a0f307798299ae02
Michael Douglas <michaeldougls93@gmail.com>
2022-08-15 12:09:44 +0100

    add surge at project

---
+++ src/pages/SignUpPage/styled.js
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+
+export const ScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  margin-top: 10vh;
+`
+
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  max-width: 450px;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export const SignUpFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  max-width: 450px;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export const LogoImage = styled.img`
+  width: 70vw;
+  max-width: 350px;
+  border-radius: 0 70% 0;
+`
